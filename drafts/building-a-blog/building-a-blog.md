bb0f8d1ba97553d388a11b7812e8eeb4da9db4c3
Joern Bernhardt <jb@campudus.com>
2017-11-30 22:37:26 +0100

    Fix lastEditedOn
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- drafts/building-a-blog/building-a-blog.md
+++ drafts/building-a-blog/building-a-blog.md
@@ -3,7 +3,20 @@
 There are so many frameworks and I just want to build stuff. It's a lot more fun than trying out all these frameworks.
 And to create my own scripted blog stuff, I should add some more test articles. Well, this is obviously one because I 
 have not really something to say other than what I'm building here right now.
- 
-## 
 
+I'll try to put my thoughts into something readable.
 
+## Creating a post
+
+This should change. For now, I'll just create a new directory under `/drafts`, call it whatever I want and put an `.md` 
+file with the same name under it. You can add meta data like title by adding a `.json` file into that directory.
+
+## Publishing a post
+
+The command `npm run publish <draft-name>` uses the `.json` and `.md` files inside the draft directory and creates a 
+post from it. It will create a `.json` if none exists and add a `createdAt` field to it with the current date.
+
+## Editing a published post
+
+If I need to change a published post, I can edit it directly and another run of `npm run publish <draft-name>` takes 
+care of updating the `lastEditedOn` field. Hopefully.
--- scripts/publish/publish-draft.js
+++ scripts/publish/publish-draft.js
@@ -4,6 +4,7 @@ const { promisify } = require("util");
 const marked = require("marked");
 
 const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 module.exports = {
   publishDraft
@@ -11,33 +12,34 @@ module.exports = {
 
 async function publishDraft(draft, config) {
   const content = (await readFile(`${config.draftsDirectory}/${draft}/${draft}.md`)).toString();
-  const metaArticle = JSON.parse(
-    (await readFile(`${config.draftsDirectory}/${draft}/${draft}.json`).catch(e => ({}))).toString()
-  );
+  const metaFile = `${config.draftsDirectory}/${draft}/${draft}.json`;
+  const metaFileString = (await readFile(metaFile).catch(e => ({}))).toString();
+  const metaArticle = JSON.parse(metaFileString);
   const html = marked(content);
-  const { day, month, year, date } = getDateFromDraft(metaArticle);
+  const { day, month, year, createdAt, lastEditedOn } = getDateFromDraft(metaArticle);
   const resultPath = `${year}/${month}/${day}/${draft}`;
-  console.log(`Publishing draft ${draft} into ${resultPath}`);
-  const generalMeta = {
-    lastEditedOn: date
-  };
-  const meta = { ...generalMeta, ...metaArticle, content: html };
+  const meta = { createdAt, ...metaArticle, lastEditedOn };
+  const article = { ...meta, content: html };
 
   let template = (await readFile(`${config.templateDirectory}/article.html`)).toString();
   const replace = `\\$\\{((?:\{.*?}|[^}])+?)\\}`;
-  template = template.replace(new RegExp(replace, "g"), (matched, key) => (meta[key] ? meta[key] : eval(key)));
+  template = template.replace(new RegExp(replace, "g"), (matched, key) => (article[key] ? article[key] : eval(key)));
 
+  console.log(`Updating draft ${draft} meta-data.`);
+  await writeFile(metaFile, `${JSON.stringify(meta, null, 2)}\n`);
+  console.log(`Publishing draft ${draft} into ${resultPath}`);
   await mkdirp(config.blogDirectory, resultPath);
   await promisify(fs.writeFile)(`${config.blogDirectory}/${resultPath}/index.html`, template);
 }
 
 function getDateFromDraft(metaArticle) {
-  const now = metaArticle.createdAt ? new Date(metaArticle.createdAt) : new Date();
-  const year = now.getUTCFullYear();
-  const month = now.getUTCMonth() + 1;
-  const day = now.getUTCDate();
+  const lastEditedOn = new Date();
+  const createdAt = metaArticle.createdAt ? new Date(metaArticle.createdAt) : lastEditedOn;
+  const year = createdAt.getUTCFullYear();
+  const month = createdAt.getUTCMonth() + 1;
+  const day = createdAt.getUTCDate();
 
-  return { day, month, year, date: now };
+  return { day, month, year, createdAt, lastEditedOn };
 }
 
 async function mkdirp(workingDir, path) {
